598d9b3a5392b2e194a780bf53e9d4f83918533f
Thomas LAMARCHE <thomas.lamarche@randstaddigital.fr>
2024-10-07 15:51:00 +0200

    fix alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 // import { defineConfig } from 'vite'
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
-    environment: 'jsdom',
-    setupFiles: 'setupTests.ts'
-  }
-})
+    environment: "jsdom",
+    setupFiles: "setupTests.ts",
+  },
+});
