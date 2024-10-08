832e2137f62ad75582bc0eda7c5c4246604869bc
Thomas LAMARCHE <thomas.lamarche@randstaddigital.fr>
2024-10-08 13:12:03 +0200

    card add

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,14 +18,18 @@ function App() {
         </a>
       </div>
       <h1 className="text-4xl">Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+      <Card>
+        <Button
+          variant={"outline"}
+          size={"lg"}
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </Card>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
