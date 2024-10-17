6fbc963fd4bf59262dfe8a9a5b8e79d3f59ed67c
Thomas Lamarche <glims@wanadoo.fr>
2024-10-17 22:13:32 +0200

    fix config

---
+++ STEP.md
@@ -0,0 +1,10 @@
+Install prettier
+npm install prettier eslint-config-prettier --save-dev
+
+Install vitest
+npm install -D vitest
+
+Install Testing Library
+npm install --save-dev @testing-library/react
+
+npm install --save-dev @types/node
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        },
+    },
+    test: {
+        environment: 'jsdom',
+        setupFiles: 'setupTests.ts',
+        coverage: {
+            include: ['src/**/*'],
+            exclude: ['src/mocks', 'src/vite-env.d.ts'],
+        },
+    },
 })
