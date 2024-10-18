61cf6717ceefeb71d0754975c3d5ae5517de5bc5
Thomas Lamarche <glims@wanadoo.fr>
2024-10-18 19:03:02 +0200

    structure add

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import eslint from 'vite-plugin-eslint2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslint()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
             '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@hooks': path.resolve(__dirname, './src/hooks/'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@routes': path.resolve(__dirname, './src/routes'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@utils': path.resolve(__dirname, './src/utils'),
         },
     },
     test: {
@@ -19,7 +26,6 @@ export default defineConfig({
             include: ['src/**/*'],
             exclude: [
                 'src/mocks',
-                'src/tests',
                 'src/vite-env.d.ts',
                 'src/main.tsx',
                 'src/**/*.test.ts',
